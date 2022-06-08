d423892551caa16ad3449a7e9702f5f8fc53bc53
unknown <wakeitupacademy@gmail.com>
2022-06-08 15:49:07 +0300

    Refactored component to get data from class: one way data binding

--- wake-up-course/src/app/post/post.component.ts
+++ wake-up-course/src/app/post/post.component.ts
@@ -2,18 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-post',
-  template: `<figure class=”post”>
-      <img
-        src="https://starecat.com/content/wp-content/uploads/when-your-program-is-a-complete-mess-but-it-does-its-job-pigeon-flying.jpg"
-        alt="post">
-      <figcaption>This image is awesome!</figcaption>
-    </figure>
-    <div class=”tags-container”>
-      <span>#image</span>
-      <span>#angular</span>
-    </div>`,
-  styles: ['.post { font-weight: 700; }']
+  templateUrl: 'post.component.html',
+  styleUrls: ['post.component.scss']
 })
 export class PostComponent {
+
+  public imgSrc = 'https://starecat.com/content/wp-content/uploads/when-your-program-is-a-complete-mess-but-it-does-its-job-pigeon-flying.jpg';
+  public imgAlt = 'post';
+  public imgCaption = 'This image is awesome!';
+  public tags = ['#image', '#angular'];
 
 }
